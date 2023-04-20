2594c65ba2b861afaa3d7bdaa250d544e87004aa
rec0veryyy <alejandrooymariaa1@gmail.com>
2023-04-20 21:50:05 +0200

    Small cleanup

--- BOIIIEasyServer.bat
+++ BOIIIEasyServer.bat
@@ -1,10 +1,10 @@
 @echo off
-title "Create Easy Server for BOIII v0.1"
+title "BOIII EasyServer for BOIII v0.1"
 
-set current_path=%cd%
-set t7_server_config_url="https://codeload.github.com/Dss0/t7-server-config/zip/refs/heads/main"
-set steamcmd_file=steamcmd.zip
-set steamcmd_url="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
+set CurrentPath=%cd%
+set T7ServerConfigUrl="https://codeload.github.com/Dss0/t7-server-config/zip/refs/heads/main"
+set SteamcmdFile=steamcmd.zip
+set SteamcmdUrl="https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip"
 
 :menu
 cls
@@ -12,10 +12,10 @@ echo.
 echo +===========================================+
 echo +
 echo +  1) Install BOIII server in:
-echo +     \__ %cd%\BOIII
+echo +     \__ %CurrentPath%\BOIII
 echo +
 echo +  2) Coming soon
-echo +  3) exit
+echo +  0) exit
 echo +
 echo +===========================================+
 echo.
@@ -23,10 +23,11 @@ set /p option=">> "
 
 if %option% == 1 goto opt1
 if %option% == 2 goto opt2
-if %option% == 3 goto exitScript
+if %option% == 0 goto exitScript
 
-::Mensaje de error, validación cuando se selecciona una opción fuera de rango
-echo. Option "%option%" is not valid
+:: Error message, validation when an out-of-range option is selected
+echo.
+echo ( Option "%option%" is not valid )
 echo.
 pause
 echo.
@@ -36,38 +37,37 @@ goto menu
 cls
 echo.
 echo Installing Steamcmd...
-timeout 4 > nul
-curl -s %steamcmd_url% -o %current_path%\%steamcmd_file%
-tar -xf %steamcmd_file%
-del %steamcmd_file%
+timeout 5 > nul
+curl -s %SteamcmdUrl% -o %CurrentPath%\%SteamcmdFile%
+tar -xf %SteamcmdFile%
+del %SteamcmdFile%
 mkdir steamcmd BOIII
-move steamcmd.exe %current_path%\steamcmd
+move steamcmd.exe %CurrentPath%\steamcmd
 cd steamcmd
 cls
 echo.
 echo Downloading Black Ops 3 Server from Steamcmd...
-timeout 4 > nul
+timeout 5 > nul
 steamcmd +force_install_dir ..\BOIII +login anonymous +app_update 545990 validate +quit
 cls
-cd %current_path%\BOIII
-cls
 echo Downloading BOIII Server files from https://github.com/Dss0/t7-server-config
-timeout 4> nul
-curl -s %t7_server_config_url% -o %current_path%\BOIII\UnrankedServer\main.zip
-cd %current_path%\BOIII\UnrankedServer
-tar -xf %current_path%\BOIII\UnrankedServer\main.zip
-xcopy /s /e %current_path%\BOIII\UnrankedServer\t7-server-config-main\*.* %current_path%\BOIII\UnrankedServer
+timeout 5 > nul
+curl -s %T7ServerConfigUrl% -o %CurrentPath%\BOIII\UnrankedServer\main.zip
+cd %CurrentPath%\BOIII\UnrankedServer
+tar -xf %CurrentPath%\BOIII\UnrankedServer\main.zip
+xcopy /s /e %CurrentPath%\BOIII\UnrankedServer\t7-server-config-main\*.* %CurrentPath%\BOIII\UnrankedServer
 rmdir /s /q t7-server-config-main
 del main.zip
 echo.
 cls
 echo.
 echo.
-echo Server Installed! - Close Script
-cd %current_path%
+echo Server Installed in: %CurrentPath%\BOIII\UnrankedServer
+echo You can close the script ;)
+echo.
+cd %CurrentPath%
 pause
 goto menu
-
 
 
 :exitScript
